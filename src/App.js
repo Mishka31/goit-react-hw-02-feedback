1379d21b2bd93af6ff1fd6221048796371e7a043
Misha Krasnonos <muzishutka7@gmail.com>
2021-09-01 16:20:54 +0300

    1 part 2hw

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Feedback from "./feedback/feedback.jsx";
 function App() {
   return (
     <div className="App">
-      <p>App.js Working!</p>
       <Feedback />
     </div>
   );
--- src/feedback/feedback.jsx
+++ src/feedback/feedback.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import s from "./feedback.module.css";
+import Section from "./section/section.jsx";
+import Panel from "./panel/panel.jsx";
+import Statistic from "./statistic/statistic.jsx";
+import Notification from "./notification/notification.jsx";
 
 class Feedback extends React.Component {
   static defaultProps = {
@@ -14,49 +17,50 @@ class Feedback extends React.Component {
     procenteg: this.props.initProcent,
   };
 
-  onButtonClick1 = () => {
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
+  onButtonGood = () => {
     this.setState((s) => ({
       good: s.good + 1,
-      total: 1 + s.good + s.neutral + s.bad,
-      procenteg: Math.round((s.good / s.total) * 100),
+      total: this.countTotalFeedback(),
+      procenteg: this.countPositiveFeedbackPercentage(),
     }));
   };
-  onButtonClick2 = () => {
+  onButtonNeutral = () => {
     this.setState((s) => ({
       neutral: s.neutral + 1,
-      total: 1 + s.good + s.neutral + s.bad,
-      procenteg: Math.round((s.good / s.total) * 100),
+      total: this.countTotalFeedback(),
+      procenteg: this.countPositiveFeedbackPercentage(),
     }));
   };
-  onButtonClick3 = () => {
+  onButtonBad = () => {
     this.setState((s) => ({
       bad: s.bad + 1,
-      total: 1 + s.good + s.neutral + s.bad,
-      procenteg: Math.round((s.good / s.total) * 100),
+      total: this.countTotalFeedback(),
+      procenteg: this.countPositiveFeedbackPercentage(),
     }));
   };
 
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <div>
-        <h2>Please leave feedback</h2>
-        <div>
-          <button className={s.button} type="button" onClick={this.onButtonClick1}>
-            Good
-          </button>
-          <button className={s.button} type="button" onClick={this.onButtonClick2}>
-            Neutral
-          </button>
-          <button className={s.button} type="button" onClick={this.onButtonClick3}>
-            Bad
-          </button>
-        </div>
-        <h3>Statistic</h3>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
-        <p>Total: {this.state.total}</p>
-        <p>Positive feedback: {this.state.procenteg}%</p>
+        <Section title="Please leave feedback">
+          <Panel onGood={this.onButtonGood} onNeutral={this.onButtonNeutral} onBad={this.onButtonBad} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistic Good={good} Neutral={neutral} Bad={bad} Total={this.countTotalFeedback()} Positive={this.countPositiveFeedbackPercentage()} />
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
       </div>
     );
   }
